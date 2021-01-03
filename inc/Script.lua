a2f03c6d71d963e68654a8008d907f2ccf0b944a
LHBMXS <74975701+LHBMXS@users.noreply.github.com>
2021-01-03 14:43:31 +0300

    Add files via upload

--- inc/Script.lua
+++ inc/Script.lua
@@ -2266,7 +2266,7 @@ keyboard.inline_keyboard = {
 },
 }
 local msg_id = msg.id_/2097152/0.5
-https.request("https://api.telegram.org/bot"..token..'/sendMessage?chat_id=' .. msg.chat_id_ .. '&text=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard))
+https.request("https://api.telegram.org/bot"..token..'/sendMessage?chat_id=' .. msg.chat_id_ .. '&text=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.decode(keyboard))
 end
 end
 if MsgText[1]== 'help1' then
